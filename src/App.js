aa3aca7f2d526d490334f7f0bd9eb01d52339436
Cassindi <tara.devin.b@gmail.com>
2019-01-08 10:35:55 -0600

    stupid nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import "react-bootstrap";
 
 const App = () =>
 (
--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./SideNav.css";
 import { Nav, Col, NavItem } from "react-bootstrap";
 
+function handleSelect(selectedKey) {
+    console.log(`selected ${selectedKey}`);
+};
+
+
 const SideNav = () => (
     <Col md={2} id="sidenav">
         <div id="fixed">
@@ -9,16 +14,20 @@ const SideNav = () => (
             <p>Full-stack Web Developer</p>
             <br />
             <p>
-                <Nav bsStyle="pills" stacked activeKey={1}>
+                <Nav id="nav" bsStyle="pills" stacked onSelect={handleSelect}>
+                    <NavItem eventKey={0} href="#intro">
+                        Intro
+                    </NavItem>
                     <NavItem eventKey={1} href="#education">
                         Education
                     </NavItem>
                     <NavItem eventKey={2} href="#experience">
                         Experience
                     </NavItem>
-                    <NavItem eventKey={3} href="projects">
+                    <NavItem eventKey={3} href="#projects">
                         Projects
                     </NavItem>
+                    
                 </Nav>
             </p>
         </div>
